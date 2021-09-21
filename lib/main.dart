50d3d6f1d29782148a422b998cbb39562e193e1e
pho0m <pho0m2000s@gmail.com>
2021-09-21 19:10:09 +0700

    add new example

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       title: 'Dt-app Demo',
       theme: ThemeData(
@@ -30,37 +29,121 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {   
-      _counter++;
+  void _printEvent(String text) {
+    setState(() {
+      print(text);
     });
+  }
+
+  Widget builCalculate(String text1, String text2, String text3) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(
+                padding: const EdgeInsets.all(0.0),
+                backgroundColor: Colors.blue,
+              ),
+              onPressed: () {
+                _printEvent(text1);
+              },
+              child: Text(
+                text1,
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                padding: const EdgeInsets.all(0.0),
+                backgroundColor: Colors.green
+              ),
+              onPressed: () {
+                _printEvent(text2);
+              },
+              child: Text(
+                text2,
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                padding: const EdgeInsets.all(0.0),
+                backgroundColor: Colors.amber
+              ),
+              onPressed: () {
+                _printEvent(text3);
+              },
+              child: Text(
+                text3,
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ]),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
+      // appBar: AppBar(
+      //   title: Text(widget.title),
+      // ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: Container(
+          width: 300,
+          height: 300,
+          color: Colors.red,
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                builCalculate("7", "4", "1"),
+                builCalculate("8", "5", "2"),
+                builCalculate("9", "6", "3"),
+              ]),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ),
     );
   }
 }
+
+
+
+/*
+
+                  Text(
+                    "4",
+                    style: TextStyle(color: Colors.red),
+                  ),
+                  Text(
+                    "5",
+                    style: TextStyle(color: Colors.red),
+                  ),
+                  Text(
+                    "6",
+                    style: TextStyle(color: Colors.red),
+                  ),
+
+
+                  
+                  Text(
+                    "7",
+                    style: TextStyle(color: Colors.red),
+                  ),
+                  Text(
+                    "8",
+                    style: TextStyle(color: Colors.red),
+                  ),
+                  Text(
+                    "9",
+                    style: TextStyle(color: Colors.red),
+                  ),
+*/
