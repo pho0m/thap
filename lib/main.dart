3b36dee42371008d577a8a17fb70067c00699a7f
pho0m <pho0m2000s@gmail.com>
2021-09-23 23:45:36 +0700

    fixed build fail remove flab

--- lib/main.dart
+++ lib/main.dart
@@ -32,14 +32,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -89,15 +81,6 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ],
         ),
-      ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: _incrementCounter,
-            child: const Icon(Icons.add),
-          ),
-        ],
       ),
     );
   }
