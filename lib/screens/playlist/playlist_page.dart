5210d4d19ccecac80ca55b2c33ee37569c701869
OverloadsOP <xdireksmmana@gmail.com>
2021-09-26 21:27:18 +0700

    creat playlist page appbar and body

--- lib/screens/playlist/playlist_page.dart
+++ lib/screens/playlist/playlist_page.dart
@@ -1,32 +1,63 @@
+import 'package:dt_app/theme/constant.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 
 class PlaylistPage extends StatelessWidget {
   const PlaylistPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    double _width = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Playlist Page"),
-      ),
-      body: Center(
+        appBar: appBar(_width),
+        body: _body([
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const Text("Playlist Page"),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Go back!'),
+                ),
+              ],
+            ),
+          ),
+        ]));
+  }
+
+  Widget _body(List<Widget> inhome) {
+    return SafeArea(
+      child: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            const Text("Playlist Page"),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('Go back!'),
-            ),
-          ],
+          children: inhome,
         ),
       ),
+    );
+  }
+
+  AppBar appBar(double width) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      shadowColor: Colors.transparent,
+      title: const Text(
+        "PlayList",
+        style: head3,
+      ),
+      elevation: 0,
+      actions: [
+        IconButton(
+          color: Colors.black,
+          icon: const Icon(FeatherIcons.circle),
+          onPressed: () {},
+        ),
+      ],
     );
   }
 }
