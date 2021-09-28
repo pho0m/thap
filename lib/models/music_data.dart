6aae6ed6978e60dfeb01a62ee1e2f4a6ac5daf90
pho0m <pho0m2000s@gmail.com>
2021-09-29 01:16:02 +0700

    change build.gradle to     ext.kotlin_version = '1.5.10'

---
+++ lib/models/music_data.dart
@@ -0,0 +1,7 @@
+class MusicData {
+  final String title;
+  final String artist;
+  final String image;
+
+  MusicData({required this.title, required this.artist, required this.image});
+}
--- lib/screens/playlist/playlist_page.dart
+++ lib/screens/playlist/playlist_page.dart
@@ -1,32 +1,140 @@
+import 'package:dt_app/theme/constant.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 
 class PlaylistPage extends StatelessWidget {
-  const PlaylistPage({Key? key}) : super(key: key);
+  PlaylistPage({Key? key}) : super(key: key);
+
+  final playlistdetails =
+      PlaylistDetails(artist: "test", title: "yoloooooooooooooooo");
+
+  final List<Widget> mockPlList = [
+    Container(
+      padding: const EdgeInsets.all(8),
+      child: const Text("He'd have you all unravel at the"),
+      color: Colors.teal[100],
+    ),
+    Container(
+      padding: const EdgeInsets.all(8),
+      child: const Text('Heed not the rabble'),
+      color: Colors.teal[200],
+    ),
+    Container(
+      padding: const EdgeInsets.all(8),
+      child: const Text('Sound of screams but the'),
+      color: Colors.teal[300],
+    ),
+    Container(
+      padding: const EdgeInsets.all(8),
+      child: const Text('Who scream'),
+      color: Colors.teal[400],
+    ),
+    Container(
+      padding: const EdgeInsets.all(8),
+      child: const Text('Revolution is coming...'),
+      color: Colors.amber[500],
+    ),
+    Container(
+      padding: const EdgeInsets.all(8),
+      child: const Text('Revolution, they...'),
+      color: Colors.amber[600],
+    ),
+    Container(
+      padding: const EdgeInsets.all(8),
+      child: const Text('Revolution, they...'),
+      color: Colors.amber[600],
+    ),
+    Container(
+      padding: const EdgeInsets.all(8),
+      child: const Text('Revolution, they...'),
+      color: Colors.amber[600],
+    ),
+    Container(
+      padding: const EdgeInsets.all(8),
+      child: const Text('Revolution, they...'),
+      color: Colors.amber[600],
+    ),
+    Container(
+      padding: const EdgeInsets.all(8),
+      child: const Text('Revolution, they...'),
+      color: Colors.amber[600],
+    ),
+    Container(
+      padding: const EdgeInsets.all(8),
+      child: const Text('Revolution, they...'),
+      color: Colors.amber[600],
+    ),
+    Container(
+      padding: const EdgeInsets.all(8),
+      child: const Text('Revolution, they...'),
+      color: Colors.amber[600],
+    ),
+    Container(
+      padding: const EdgeInsets.all(8),
+      child: const Text('Revolution, they...'),
+      color: Colors.amber[600],
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
+    double _width = MediaQuery.of(context).size.width;
+    //double _height = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Playlist Page"),
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
+        elevation: 0,
+        title: const Text(
+          "Playlist Page",
+          style: head3,
+        ),
+        actions: [
+          IconButton(
+            color: Colors.black,
+            icon: const Icon(FeatherIcons.circle),
+            onPressed: () {},
+          ),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            const Text("Playlist Page"),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('Go back!'),
-            ),
-          ],
+      body: Container(
+        child: GridView.count(
+          primary: false,
+          padding: const EdgeInsets.all(20),
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          crossAxisCount: 3,
+          children: mockPlList,
         ),
       ),
     );
   }
+
+  Widget _gridContainer(double _width) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      color: Colors.grey,
+      height: 300,
+      width: _width - 30,
+      child: GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(20),
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        crossAxisCount: 3,
+        children: mockPlList,
+      ),
+    );
+  }
+}
+
+class PlaylistDetails {
+  String title;
+  String artist;
+
+  PlaylistDetails({
+    required this.title,
+    required this.artist,
+  });
 }
