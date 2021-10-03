88296694bf9182a3b00942b0da507c23755c039a
pho0m <pho0m2000s@gmail.com>
2021-10-03 15:00:23 +0700

    fixed homepage add new navigator to play mockMusic

--- lib/models/music_data.dart
+++ lib/models/music_data.dart
@@ -2,6 +2,12 @@ class MusicData {
   final String title;
   final String artist;
   final String image;
+  final String musicPlay;
 
-  MusicData({required this.title, required this.artist, required this.image});
+  MusicData({
+    required this.title,
+    required this.artist,
+    required this.image,
+    required this.musicPlay,
+  });
 }
--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -24,6 +24,11 @@ class _HomePageState extends State<HomePage> {
   ];
 
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     //double _width = MediaQuery.of(context).size.width;
     //double _height = MediaQuery.of(context).size.height;
@@ -43,9 +48,9 @@ class _HomePageState extends State<HomePage> {
               sizeBoxs20,
               favorite(),
               sizeBoxs20,
-              play(),
             ],
           ),
+          testPlay(),
         ],
       ),
     );
@@ -71,19 +76,48 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget play() {
-    return ElevatedButton(
-      child: const Text("Play"),
-      style: ElevatedButton.styleFrom(
-        primary: Colors.grey,
-        padding: const EdgeInsets.all(20),
+  Widget testPlay() {
+    final MusicData mockData = MusicData(
+      title: "if you shy (let me knows)",
+      artist: "1975",
+      image:
+          "https://images.squarespace-cdn.com/content/v1/56858337cbced60d3b293aef/1572288107885-V2AZJF8YVG5NARZRU7YE/Albumism_The1975_ABriefInquiryIntoOnlineRelationships_MainImage.png.jpg?format=1000w",
+      musicPlay: "musics/testmusic.mp3",
+    );
+
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(25.15),
+        ),
+        color: Colors.grey[300],
+        child: ListTile(
+          leading: const Icon(
+            FeatherIcons.headphones,
+            size: 40.0,
+          ),
+          // leading: const FlutterLogo(size: 56.0),
+          title: Text(
+            mockData.title,
+            style: head4,
+          ),
+          subtitle: Text(
+            mockData.artist,
+            style: sub1,
+          ),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => PlayerPage(
+                  musicData: mockData,
+                ),
+              ),
+            );
+          },
+        ),
       ),
-      onPressed: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => const PlayerPage()),
-        );
-      },
     );
   }
 
