bcad88b0ee05bdaf0ef5f17aeec0e540414992be
phuwis <phuwis.tweed@gmail.com>
2021-10-11 03:32:30 +0700

    fix error problem

--- lib/screens/lyrics/components.dart/lyrics_container.dart
+++ lib/screens/lyrics/components.dart/lyrics_container.dart
@@ -1,10 +1,7 @@
-import 'package:dt_app/models/models.dart';
 import 'package:flutter/material.dart';
 
-import '../../route.dart';
-
 class LyricsContainer extends StatefulWidget {
-  LyricsContainer({Key? key}) : super(key: key);
+  const LyricsContainer({Key? key}) : super(key: key);
 
   @override
   _LyricsContainerState createState() => _LyricsContainerState();
