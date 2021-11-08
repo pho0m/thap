3345db27adf879c0ac1bb097d2f3392300d225cc
phuwis <phuwis.tweed@gmail.com>
2021-11-08 23:37:36 +0700

    fixed in artist-page form to beta

--- lib/mock/artist.dart
+++ lib/mock/artist.dart
@@ -3,32 +3,43 @@ import 'package:dt_app/models/models.dart';
 //mockArtistOka Artist 'OKAMOTO' data
 final Artist mockArtistOka = Artist(
   name: "okamoto",
-  image: "https://i.ytimg.com/vi/qRrwK84G3fg/maxresdefault.jpg",
+  imageArtist: "https://i.ytimg.com/vi/qRrwK84G3fg/maxresdefault.jpg",
+  imageSub: "https://i.ytimg.com/vi/js05ruSiGZU/maxresdefault.jpg",
   history:
       "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.",
+  dataArtist: "It is a long established fact that a reader will be distracted ",
 );
 
 //mockArtistPREP Artist 'PREP' data
 final Artist mockArtistPREP = Artist(
   name: "PREP",
-  image:
+  imageArtist:
+      "https://2.bp.blogspot.com/-SnboENv4D9U/W9saA4YGcVI/AAAAAAAAFNk/-4nceBodbIIIN4T1rVeLj8qZmo2u4-dDACLcBGAs/s1600/PREP%2Bpress%2Bshot%2Bsmall%2Bcredit%2BKenya%2BAbe-0001.jpg",
+  imageSub:
       "https://sgp1.digitaloceanspaces.com/adaybulletin/2020/11/FB-The-Guest-Prep.jpg",
   history:
       "Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of `de Finibus Bonorum et Malorum` (The Extremes of Good and Evil) by Cicero, written in 45 BC.",
+  dataArtist: "It is a long established fact that a reader will be distracted ",
 );
 
 //mockArtist1975 Artist 'The 1975' data
 final Artist mockArtist1975 = Artist(
   name: "The 1975",
-  image:
+  imageArtist:
+      "https://www.kiss.ie/app/uploads/2019/10/Screen-Shot-2019-10-21-at-10.25.29-1.png",
+  imageSub:
       "https://thestandard.co/wp-content/uploads/2019/01/The-1975-COVER-WEB.jpg",
   history:
       "It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable.",
+  dataArtist:
+      "It is a long established fact that a reader will be distracted jdfigiguigb kjfgbdkjhrbkujrugbdkjhgfiuergrdfgug fdjhgiufdhiughieudehrugyvgdbuitybeuirgthuydegruytguyirygvbtuyergyuyvtgeuyirgvbuyergvuygerubyitgruytevgvuybbgruyegbeyugyu",
 );
 
 //mockNoArtist No Artist Data
 final Artist mockNoArtist = Artist(
   name: "",
-  image: "",
+  imageArtist: "",
+  imageSub: "",
   history: "",
+  dataArtist: "",
 );
