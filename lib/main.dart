b458c6d7ebd86dc3b82f9d750ad5921c59fbee5d
pho0m <pho0m2000s@gmail.com>
2021-11-20 11:16:28 +0700

    add translate

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:flutter_translate/flutter_translate.dart';
 import 'package:thap_mobile/screens/route.dart';
 import 'package:thap_mobile/theme/constant.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:lottie/lottie.dart';
 
-void main() => runApp(const ThapMobile());
+void main() async {
+  // var delegate = await LocalizationDelegate.create(
+  //     fallbackLocale: 'en', supportedLocales: ['en', 'th']);
+
+  //runApp(LocalizedApp(delegate, const ThapMobile()));
+  runApp(const ThapMobile());
+}
 
 class ThapMobile extends StatefulWidget {
   const ThapMobile({Key? key}) : super(key: key);
@@ -34,6 +41,7 @@ class _ThapMobileState extends State<ThapMobile> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    // var localizationDelegate = LocalizedApp.of(context).delegate;
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -64,5 +72,43 @@ class _ThapMobileState extends State<ThapMobile> with TickerProviderStateMixin {
         ),
       ),
     );
+
+    // LocalizationProvider(
+    //   state: LocalizationProvider.of(context).state,
+    //   child:
+
+    //   MaterialApp(
+    //     // localizationsDelegates: [localizationDelegate],
+    //     // supportedLocales: localizationDelegate.supportedLocales,
+    //     // locale: localizationDelegate.currentLocale,
+    //     debugShowCheckedModeBanner: false,
+    //     theme: ThemeData(
+    //       fontFamily: dtFontFamily,
+    //       primarySwatch: dtPrimaryColor,
+    //       textTheme: textTheme,
+    //     ),
+    //     home: Scaffold(
+    //       body: Center(
+    //         child: Builder(
+    //           builder: (context) => Lottie.asset(
+    //             'assets/lottie/music-loader.json',
+    //             controller: _controller,
+    //             height: 500,
+    //             animate: true,
+    //             onLoaded: (composition) {
+    //               _controller
+    //                 ..duration = composition.duration
+    //                 ..forward().whenComplete(() => Navigator.pushReplacement(
+    //                       context,
+    //                       MaterialPageRoute(
+    //                           builder: (context) => const HomePage()),
+    //                     ));
+    //             },
+    //           ),
+    //         ),
+    //       ),
+    //     ),
+    //   ),
+    // );
   }
 }
